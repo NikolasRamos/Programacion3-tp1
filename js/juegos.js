f65d8f0fc04827ed0e9441780f8eb8e35e6aeb67
Nikolas <nikolasr010605@gmail.com>
2026-05-11 22:56:08 -0300

    Agrego juegos.js y reorganizo todos los index con sus css

---
+++ js/juegos.js
@@ -0,0 +1,34 @@
+const cardContainer = document.querySelector('#card-container')
+
+async function getJuegos() {
+    try {
+        const response = await fetch('http://localhost:3000/juegos')
+        const data = await response.json()
+        console.log(response)
+        console.log(data)
+
+        data.forEach((juegos) => {
+            const div = document.createElement('div')
+            div.classList.add('card')
+
+            div.innerHTML = `
+            <div class="contenedor-img">
+                <img class="card-img"
+                    src="${juegos.img}"
+                    alt="${juegos.nombre}"
+                />
+            </div>
+            <div class="card-titulo"><h3>$ ${juegos.nombre}</h3></div>
+            <div class="card-valor"><h3>$ ${juegos.precio}</h3></div>
+            <div class="card-descripcion"><h3>${juegos.desc}</h3></div>
+            `
+
+            cardContainer.append(div)
+        });
+
+    } catch (error) {
+        console.log(`Error, no se pudieron traer los datos de los juegos ${error}`)
+    }
+}
+
+getJuegos()
