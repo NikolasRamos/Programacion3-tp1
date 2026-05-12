2a922204496e83e125fd470177e71e60a93a7bc9
Nikolas <nikolasr010605@gmail.com>
2026-05-12 20:39:32 -0300

    feature: Agrego el url de la api desplegada en Render

--- js/juegos.js
+++ js/juegos.js
@@ -2,7 +2,7 @@ const cardContainer = document.querySelector('#card-container')
 
 async function getJuegos() {
     try {
-        const response = await fetch('http://localhost:3000/juegos')
+        const response = await fetch('https://programacion3-tp3.onrender.com')
         const data = await response.json()
         console.log(response)
         console.log(data)
