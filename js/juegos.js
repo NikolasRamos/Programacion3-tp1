ccfe08247ceb54c7a2d7b77e81564149e4ab317c
Nikolas <nikolasr010605@gmail.com>
2026-05-13 17:03:33 -0300

    feat: Agrego buscador en servicios.html

--- js/juegos.js
+++ js/juegos.js
@@ -1,49 +1,69 @@
-const cardContainer = document.querySelector('#card-container')
+const cardContainer = document.querySelector('#card-container');
+const buscador = document.querySelector('#buscador');
+let todosLosJuegos = [];
 
+// Muestra los juegos
+function renderizarJuegos(lista) {
+    cardContainer.innerHTML = '';
+
+    lista.forEach((juego) => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.id = `id-${juego.id}`;
+
+        div.innerHTML = `       
+            <div class="contenedor-img">
+                <img class="card-img"
+                    src="${juego.img}"
+                    alt="${juego.nombre}"
+                />
+            </div>
+            <div class="card-titulo"><h3>${juego.nombre}</h3></div>
+            <div class="card-valor"><h3>${juego.precio}</h3></div>
+            <div class="card-descripcion"><h3>${juego.descripcion}</h3></div>
+            <div class="card-genero"><h3>${juego.genero}</h3></div>
+        `;
+        cardContainer.append(div);
+    });
+}
+
+// Loader
 async function getJuegos() {
     try {
-        // loader
-        if (!cardContainer) return
+        if (!cardContainer) return;
+        
         cardContainer.innerHTML = `
             <div class="loader-container">
                 <div class="loader"></div>
                 <p>Cocinando los datos, por favor espera...</p>
             </div>
-        `
-        // fin loader
+        `;
 
-        const response = await fetch('https://programacion3-tp3.onrender.com/juegos')
-        const data = await response.json()
-
-        cardContainer.innerHTML = ` `
-
-        console.log(response)
-        console.log(data)
-
-        data.forEach((juegos) => {
-            const div = document.createElement('div')
-            div.classList.add('card')
-            div.id = `id-${juegos.id}`
-
-            div.innerHTML = `       
-            <div class="contenedor-img">
-                <img class="card-img"
-                    src="${juegos.img}"
-                    alt="${juegos.nombre}"
-                />
-            </div>
-            <div class="card-titulo"><h3>${juegos.nombre}</h3></div>
-            <div class="card-valor"><h3>${juegos.precio}</h3></div>
-            <div class="card-descripcion"><h3>${juegos.descripcion}</h3></div>
-            <div class="card-genero"><h3>${juegos.genero}</h3></div>
-            `
-
-            cardContainer.append(div)
-        });
+        const response = await fetch('https://programacion3-tp3.onrender.com/juegos');
+        const data = await response.json();
+        
+        todosLosJuegos = data;
+        renderizarJuegos(todosLosJuegos);
 
     } catch (error) {
-        console.log(`Error, no se pudieron traer los datos de los juegos ${error}`)
+        cardContainer.innerHTML = `<p>Error al cargar los juegos.</p>`;
+        console.log(`Error, no se pudieron traer los datos: ${error}`);
     }
 }
 
-getJuegos()
+buscador.addEventListener('input', (e) => {
+    const busqueda = e.target.value.toLowerCase().trim();
+
+    const filtrados = todosLosJuegos.filter(juego => {
+
+        if (!isNaN(busqueda) && busqueda !== "") {
+            return juego.id.toString() === busqueda;
+        } 
+        
+        return juego.nombre.toLowerCase().includes(busqueda);
+    });
+
+    renderizarJuegos(filtrados);
+});
+
+getJuegos();
