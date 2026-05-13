b15713a9ec95cee13a3325d6d0ac517094ffb092
Nikolas <nikolasr010605@gmail.com>
2026-05-13 14:48:47 -0300

    feat: Agrego el loader en juegos.js y modifico el css de servicios

--- js/juegos.js
+++ js/juegos.js
@@ -2,8 +2,21 @@ const cardContainer = document.querySelector('#card-container')
 
 async function getJuegos() {
     try {
+        // loader
+        if (!cardContainer) return
+        cardContainer.innerHTML = `
+            <div class="loader-container">
+                <div class="loader"></div>
+                <p>Cocinando los datos, por favor espera...</p>
+            </div>
+        `
+        // fin loader
+
         const response = await fetch('https://programacion3-tp3.onrender.com/juegos')
         const data = await response.json()
+
+        cardContainer.innerHTML = ` `
+
         console.log(response)
         console.log(data)
 
@@ -18,9 +31,9 @@ async function getJuegos() {
                     alt="${juegos.nombre}"
                 />
             </div>
-            <div class="card-titulo"><h3>$ ${juegos.nombre}</h3></div>
-            <div class="card-valor"><h3>$ ${juegos.precio}</h3></div>
-            <div class="card-descripcion"><h3>${juegos.desc}</h3></div>
+            <div class="card-titulo"><h3>${juegos.nombre}</h3></div>
+            <div class="card-valor"><h3>${juegos.precio}</h3></div>
+            <div class="card-descripcion"><h3>${juegos.descripcion}</h3></div>
             `
 
             cardContainer.append(div)
